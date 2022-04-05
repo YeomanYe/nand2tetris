69d5bcaa40e2c90ce9bb11c4db61ca0f1089ecbd
yeming <yeming03@baidu.com>
2022-04-05 20:32:28 +0800

    feat: update multi

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,17 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@0
+R0=A
+(LOOP)
+R2=R2+R1
+@1
+R0=R0-A
+@END
+RO;JEQ
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
